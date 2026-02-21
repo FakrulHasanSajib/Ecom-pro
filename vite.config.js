bd1fbc74d94035850d0181d4b365875b803c51de
Fakrul Hasan Sajib <ahmedsajib1999s@gmail.com>
2026-02-21 13:36:24 +0600

    error

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite'; // এটি যোগ করুন
+import path from 'path'; // এটি ইমপোর্ট করতে হবে
 
 export default defineConfig({
     plugins: [
-        tailwindcss(), // এটি সবার উপরে যোগ করুন
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         vue(),
     ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js', // এই লাইনটি যোগ করুন
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
 });
